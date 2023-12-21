e6b90078351d193a4f5aa303052c69a5fa2e2759
Alisamar Husain <zrthxn@gmail.com>
2023-12-21 14:19:17 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='upycli',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     description='A microscopic library to turn any function into a CLI.',
     long_description=open('README.md', 'r').read(),
