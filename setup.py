9716aae78bf7c56b87af2e8793836aac7218078b
Alisamar Husain <zrthxn@gmail.com>
2023-12-21 13:54:07 +0100

    Readme

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    README = f.read()
+
 setup(
     name='upycli',
     version='0.1.0',
     packages=find_packages(),
     description='A microscopic library to turn any function into a CLI.',
+    long_description=README,
     entry_points = {
         'console_scripts': ['ucli=upycli.runner:run'],
     }
